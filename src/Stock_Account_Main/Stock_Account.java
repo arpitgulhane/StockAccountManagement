9e80c0b50f932324fd5b62227c6b48d9935a44aa
DESKTOP-VFH6PON\USER <arpit@edupluscampus.com>
2023-03-03 18:20:47 +0530

    changes

--- src/Stock_Account_Main/Stock_Account.java
+++ src/Stock_Account_Main/Stock_Account.java
@@ -1,24 +1,30 @@
 package Stock_Account_Main;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Stock_Account {
+    public static ArrayList<Stock> stock_array=new ArrayList();
+
     public static void main(String[] args) {
         System.out.println("Enter N Number Of Stock : ");
         Scanner N_st=new Scanner(System.in);
         int Num=N_st.nextInt();
-        Stock st=new Stock();
 
         int sharePrice,numberOfShare;
         String name;
         Scanner sc=new Scanner(System.in);
 
+
         for (int i=0;i<Num;i++){
+//            Stock stock=new Stock();
             System.out.println("Enter Share Name,Number Of Share,Share Price :");
             name = sc.nextLine();
             sharePrice = sc.nextInt();
             numberOfShare = sc.nextInt();
-            st.StockCalculate(name,numberOfShare,sharePrice);
+            Stock stock = new Stock(name,numberOfShare,sharePrice);
+//            stock(name,numberOfShare,sharePrice);
+            stock_array.add(stock);
 //            Name, Number of
 //            Share, and Share Price
         }
@@ -28,12 +34,35 @@ public class Stock_Account {
 }
 
 class Stock{
+//    void StockCalculate(String name,int numberOfShare,int sharePrice){
+        String shareName ;
+        int numberShare;
+        int sharePrise;
+        int value;
 
-    void StockCalculate(String name,int numberOfShare,int sharePrice){
-        int total=numberOfShare+sharePrice;
-        System.out.println(name+" Total : "+total);
+    public Stock(String name, int numberOfShare, int sharePrice){
+            this.shareName=shareName;
+            this.numberShare=numberShare;
+            this.sharePrise=sharePrise;
+            this.value=numberShare*sharePrise;
+    }
+
+    public String getShareName(){
+            return this.shareName;
+    }
+    public int getNumberDhare(){
+            return this.numberShare;
+    }
+    public int getSharePrise(){
+        return this.sharePrise;
+    }
+    public int getValue(){
+        return this.value;
+    }
+//        int value=numberOfShare+sharePrice;
+//        System.out.println(name+" Total : "+total);
 
     }
 
-    }
+//    }
 
