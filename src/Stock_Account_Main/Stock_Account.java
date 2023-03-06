460d9b29d19b7736739a2a64e7c07a010b2c91d3
DESKTOP-VFH6PON\USER <arpit@edupluscampus.com>
2023-03-06 14:55:32 +0530

    Commercial data processing

--- src/Stock_Account_Main/Stock_Account.java
+++ src/Stock_Account_Main/Stock_Account.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Stock_Account {
-    public static ArrayList<Stock> stock_array=new ArrayList();
+    public static ArrayList<Stock> stocks_array = new ArrayList<>();
+    public static ArrayList<Stock> stock_array=new ArrayList<>();
 
     public static void main(String[] args) {
         System.out.println("Enter N Number Of Stock : ");
@@ -15,24 +16,52 @@ public class Stock_Account {
         String name;
         Scanner sc=new Scanner(System.in);
 
-
         for (int i=0;i<Num;i++){
 //            Stock stock=new Stock();
             System.out.println("Enter Share Name,Number Of Share,Share Price :");
-            name = sc.nextLine();
+            name = sc.next();
             sharePrice = sc.nextInt();
             numberOfShare = sc.nextInt();
-            Stock stock = new Stock(name,numberOfShare,sharePrice);
+            Stock buy = new Stock(name,numberOfShare,sharePrice);
 //            stock(name,numberOfShare,sharePrice);
-            stock_array.add(stock);
-//            Name, Number of
-//            Share, and Share Price
+            stock_array.add(buy);
+            System.out.println("Enter Share Name,Number Of Share,Share Price to sell:");
+            name = sc.next();
+            sharePrice = sc.nextInt();
+            numberOfShare = sc.nextInt();
+            sell(name,numberOfShare,sharePrice);
         }
+        StockPortfolio();
+    }
 
+    //-----------------------------------------------
+    public static void StockPortfolio() {
+        double totalValue=0.0;
+        System.out.println("Stock Report :");
+    for(Stock stock : stock_array){
+        System.out.println(stock.getShareName());
+        System.out.println(stock.getSharePrise());
+        System.out.println(stock.getNumberShare());
+        totalValue = totalValue+stock.getValue();
+        }
+        System.out.println("Total Value of Stock :"+totalValue);
+    }
+
+    public static void Sell(String name,int numberOfShare,int sharePrice){
+        for(Stock stock : stock_array){
+            stock.setShareName(name);
+            stock.setNumberShare(numberOfShare);
+            stock.setSharePrise(sharePrice);
+        }
+        System.out.println("Stock sell..");
+        }
 
     }
 }
 
+
+
+//-----------------------------------------------
 class Stock{
 //    void StockCalculate(String name,int numberOfShare,int sharePrice){
         String shareName ;
@@ -50,7 +79,7 @@ class Stock{
     public String getShareName(){
             return this.shareName;
     }
-    public int getNumberDhare(){
+    public int getNumberShare(){
             return this.numberShare;
     }
     public int getSharePrise(){
@@ -62,7 +91,21 @@ class Stock{
 //        int value=numberOfShare+sharePrice;
 //        System.out.println(name+" Total : "+total);
 
+    public String setShareName(String ShareName){
+        this.shareName=shareName;
+        return ShareName;
     }
-
-//    }
+    public int setNumberShare(int NumberShare){
+        this.numberShare=NumberShare-numberShare;
+        return numberShare;
+    }
+    public int setSharePrise(int sharePrise){
+        this.sharePrise=sharePrise-sharePrise;
+        return sharePrise;
+    }
+    public int setValue(int value){
+        this.value=value-value;
+        return this.value;
+    }
+    }
 
